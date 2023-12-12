4ae80e7e05496e2a904d29608821593e80886b21
Shmoopey <davec1153@hotmail.com>
2023-12-12 19:38:11 +0000

    Add project files.

---
+++ ExternalPlugins/scripts/AutoDivination.cpp
@@ -0,0 +1,163 @@
+#include <iostream>
+#include "../source/API.h"
+#include <random>
+#include <filesystem>
+#include <thread>
+using namespace std;
+
+
+static const int MAX_IDLE_TIME_MINUTES = 5;
+
+static time_t startTime = std::time(nullptr);
+static time_t afk = std::time(nullptr);
+static int currentCount;
+static int totalGained;
+
+//18204 chronicle fragment, other peopls 18205
+//19884 guthix butterfly       	
+//26022 seren spirit
+//27228 Divine blessing
+//27297 Forge phoenix
+//28411 catalyst
+//30599 h Pumpkin 
+//15451 Fire spirit
+//find random events, and click them
+static bool RandomEvents() {
+	std::vector <AllObject> F_obj = ME::GetAllObjArrayInteract({ 19884, 26022, 27228, 27297, 28411, 30599, 15451, 18204 }, 20, 1);
+	if (!F_obj.empty()) {
+		std::cout << "Random event object detected: trying to click" << std::endl;
+		//if(ME::Math_AO_ValueEqualsArr({18204,18205,19884,26022,27228,27297,28411,30599 }, F_obj)) {//if seperation is needed
+		if (DO::DoAction_NPC__Direct(0x29, OFF_ACT::InteractNPC_route, F_obj.front())) {
+			ME::RandomSleep2(1500, 4050, 12000);
+			return true;
+		}
+	}
+	return false;
+}
+
+static void idleCheck() {
+	time_t currentTime = std::time(nullptr);
+	time_t timeDiff = currentTime - afk;
+	time_t randomTime = rand() % (static_cast<int>((MAX_IDLE_TIME_MINUTES * 60) * 0.3)) + (MAX_IDLE_TIME_MINUTES * 60 * 0.6);
+
+	if (timeDiff > randomTime) {
+		ME::PIdle2();
+		afk = currentTime;
+	}
+}
+
+
+static int getGainedCount(int start, int current) {
+	return current - start;
+}
+
+//UI
+static ImColor purpCol = ImColor(120, 0, 128);
+static ImColor backCol = ImColor(0, 0, 0, 120);
+
+
+static void DrawGui() {
+	IG_answer backDrop{};
+	backDrop.box_name = "backDrop";
+	backDrop.box_start = { 0, 50, 0 };
+	backDrop.box_size = { 300, 200, 0 };
+	backDrop.colour = backCol;
+
+	IG_answer titleText{};
+	titleText.box_name = "titleText";
+	titleText.box_start = { 70, 65, 0 };
+	titleText.colour = purpCol;
+
+	while (LoopyLoop && !Endall) {
+
+		//RunTime
+		std::time_t currentTime = std::time(nullptr);
+
+		//run time in seconds
+		int runTime = static_cast<int>(std::difftime(currentTime, startTime));
+
+		//hh:mm:ss format
+		int hours = runTime / 3600;
+		int minutes = (runTime % 3600) / 60;
+		int seconds = runTime % 60;
+
+		//run time as string
+		std::ostringstream oss;
+		oss << std::setfill('0') << std::setw(2) << hours << ":"
+			<< std::setfill('0') << std::setw(2) << minutes << ":"
+			<< std::setfill('0') << std::setw(2) << seconds;
+		std::string timeStr = oss.str();
+
+
+
+		//UI
+		IG::DrawSquareFilled(true, &backDrop);
+		titleText.string_value = "Timer: " + timeStr;
+		IG::DrawTextAt(false, &titleText);
+
+		Sleep(1000);
+	}
+}
+
+
+void AutoDivination() {
+
+	static int startCount = ME::InvItemcountStack_String("energy");
+	vector <AllObject>EnergyRiftOpen;
+	vector <AllObject>EnergyRift;
+	vector <AllObject>EnrichedSpring;
+	vector <AllObject>RegularSpring;
+
+	std::thread guiLoop(DrawGui);
+	guiLoop.detach();
+
+	while (LoopyLoop && !Endall) {
+		idleCheck();
+		RandomEvents();
+		currentCount = ME::InvItemcountStack_String("energy");
+		totalGained = getGainedCount(startCount, currentCount);
+		ScripCuRunning2 = "Energy Gained: ";
+		ScripCuRunning2.append(to_string(totalGained));
+		ScripCuRunning1 = "Waiting";
+
+
+		if (ME::InvFull_()) {
+			ScripCuRunning1 = "Looking to convert";
+			EnergyRiftOpen = ME::GetAllObjArrayInteract({ 93489 }, 40, 0);
+			if (!EnergyRiftOpen.empty()) {
+				Sleep(300 + rand() % 300);
+				DO::DoAction_Object_Direct(0xc8, OFF_ACT::GeneralObject_route0, EnergyRiftOpen.front());
+				Sleep(300 + rand() % 300);
+			}
+			else {
+				EnergyRift = ME::GetAllObjArrayInteract({ 87306 }, 40, 0);
+				if (!EnergyRift.empty()) {
+					Sleep(300 + rand() % 300);
+					DO::DoAction_Object_Direct(0xc8, OFF_ACT::GeneralObject_route0, EnergyRift.front());
+					Sleep(300 + rand() % 300);
+				}
+			}
+			Sleep(500 + rand() % 500);
+		}
+		else {
+			if (!ME::CheckAnim(100) && !ME::ReadPlayerMovin()) {
+				ScripCuRunning1 = "Catching energy";
+				EnrichedSpring = ME::GetAllObjArrayInteract_str({ "Enriched" }, 40, 1);
+				if (!EnrichedSpring.empty()) {
+					Sleep(300 + rand() % 300);
+					DO::DoAction_NPC__Direct(0xc8, OFF_ACT::InteractNPC_route, EnrichedSpring.front());
+					Sleep(300 + rand() % 300);
+				}
+				else {
+					RegularSpring = ME::GetAllObjArrayInteract_str({ "spring","wisp" }, 40, 1);
+					if (!RegularSpring.empty()) {
+						Sleep(300 + rand() % 300);
+						DO::DoAction_NPC__Direct(0xc8, OFF_ACT::InteractNPC_route, RegularSpring.front());
+						Sleep(300 + rand() % 300);
+					}
+				}
+			}
+		}
+		Sleep(1000);
+	}
+}
